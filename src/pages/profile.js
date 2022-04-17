d1a633ebddc38c05bd0b7d96db5e89ed8a13c5ac
jongminshin <ace4463@gmail.com>
2022-04-17 21:49:05 +0900

    Add FollowList, NicknameEditForm

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,7 +1,26 @@
+import React from "react";
+import Head from 'next/head';
+
+import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
+
 
 const Profile = () => {
+    const followerList = [{ nickname: 'nick'}, {nickname: 'nick2'}, {nickname: 'nick3'}];
+    const followingList = [{ nickname: 'nick'}, {nickname: 'nick2'}, {nickname: 'nick3'}];
+
     return (
-        <div>내 프로필</div>
+        <>
+            <Head>
+                <div>내 프로필</div>
+            </Head>
+            <AppLayout>
+                <NicknameEditForm />
+                <FollowList header="팔로잉 목록" data={followingList}/>
+                <FollowList header="팔로워 목록" data={followerList}/>
+            </AppLayout>
+        </>
     );
 }
 
