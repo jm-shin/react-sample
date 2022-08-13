afb8bc36e22a7be0b13b693f9fb922f289b25e3c
jongminshin <ace4463@gmail.com>
2022-08-13 14:03:41 +0900

    feat: singup page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,23 +2,16 @@ import React, {useCallback, useState} from 'react';
 import Link from "next/link";
 import {Button, Form, Input} from "antd";
 import styled from 'styled-components';
+import PropTypes from "prop-types";
+import useInput from "../hooks/useInput";
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
 `;
 
 const LoginForm = ({setIsLoggedIn}) => {
-    const [id, setId] = useState('');
-    const [password, setPassword] = useState('');
-
-    const onChangeId = useCallback((e) => {
-        setId(e.target.value);
-    }, []);
-
-    const onChangePassword = useCallback((e) => {
-        setPassword(e.target.value);
-    }, []);
-
+    const [id, onChangeId] = useInput('');
+    const [password, onChangePassword] = useInput('');
 
     const onSubmitForm = useCallback(() => {
         console.log(id, password);
@@ -48,6 +41,10 @@ const LoginForm = ({setIsLoggedIn}) => {
             </ButtonWrapper>
         </Form>
     );
+}
+
+LoginForm.propTypes = {
+    setIsLoggedIn: PropTypes.func.isRequired,
 }
 
 export default LoginForm;
